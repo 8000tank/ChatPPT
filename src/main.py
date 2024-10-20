a6b591962553e692c27ddfc9055674cd49da7736
8000tank <8000tank@gmail.com>
2024-10-20 22:19:17 +0800

    1. 基于v0.1的作业（用新母版生成ppt） 2. 修复未创建output目录bug

--- src/main.py
+++ src/main.py
@@ -5,24 +5,49 @@ from template_manager import load_template, get_layout_mapping, print_layouts
 
 def main():
     input_text = """
-    # ChatPPT_Demo
+    # ChatPPT_Demo2(LGBTQIA)
 
-    ## ChatPPT Demo [Title Only]
+    ## ChatPPT Demo2 LGBTQIA [Title]
 
-    ## 2024 业绩概述 [Title and Content]
-    - 总收入增长15%
-    - 市场份额扩大至30%
-
-    ## 业绩图表 [Title and Picture 1]
-    ![业绩图表](images/performance_chart.png)
-
-    ## 新产品发布 [Title and 2 Column]
-    - 产品A: 特色功能介绍
-    - 产品B: 市场定位
-    ![未来增长](images/forecast.png)
+    ## Right Pattern Content [Right Pattern Content]
+    - Right Pattern Content 1
+    - Right Pattern Content 2
+    
+    ## Overview [Overview]
+    - Overview 1
+    - Overview 2
+    
+    ## Chart Slide [Chart Slide]
+    - Chart Slide 1
+    - Chart Slide 2
+    
+    ## Left Pattern Content [Left Pattern Content]
+    - Left Pattern Content 1
+    - Left Pattern Content 2
+    
+    ## Smart Art [Smart Art]
+    - Smart Art 1
+    - Smart Art 2
+            
+    ## Two Photo Content [Two Photo Content]
+    - Two Photo Content 1
+    ![Two Photo Content 2](images/forecast.png)
+    ![Two Photo Content 1](images/performance_chart.png)
+    
+    ## Right Pattern Content Blue title [Right Pattern Content Blue title]
+    - Right Pattern Content Blue title 1
+    - Right Pattern Content Blue title 2
+    
+    ## Questions [Questions]
+    - Questions 1
+    - Questions 2    
+    
+    ## Left Pattern Content Orange Title [Left Pattern Content Orange Title]
+    - Left Pattern Content Orange Title 1
+    - Left Pattern Content Orange Title 2    
     """
 
-    template_file = 'templates/MasterTemplate.pptx'
+    template_file = 'templates/LGBTQIA Pride Month presentation.pptx'
     prs = load_template(template_file)
 
     print("Available Slide Layouts:")
@@ -33,6 +58,10 @@ def main():
     powerpoint_data, presentation_title = parse_input_text(input_text, layout_mapping)
 
     output_pptx = f"outputs/{presentation_title}.pptx"
+    
+    # 添加以下代码来创建 outputs 目录（如果不存在）
+    os.makedirs(os.path.dirname(output_pptx), exist_ok=True)
+    
     generate_presentation(powerpoint_data, template_file, output_pptx)
 
 if __name__ == "__main__":
